7312800c0995b97bdff3021704325602c0a07e65
giuliagal93 <peke@MacBook-Air.local>
2016-12-11 15:45:15 +0100

    10

--- sketch.js
+++ sketch.js
@@ -29,10 +29,9 @@ function deviceTurned() {
 }
 
 function draw() {
-    background(hour()*21, minute()*4, second()*4);
+   background(hour()*21, minute()*4, second()*4);
     
 
-    push();
     
    /* noStroke();
     fill(255);
@@ -40,23 +39,23 @@ function draw() {
     rect(width/2,height/2-20,height/8,height/2)
 */
     fill(value);
+    angleMode(DEGREES);
+    fill(30)
+  
+    translate(width/2,height/3);
     
-    if (state == true) {
-        rotate(PI);
-    } else if (state == false) {
-        rotate(-PI);
-    }
+    if (state == true) { rotate(-90) }
     
-    translate(0,-1*height/100);
+    if (state == false) { translate(0,100)}
+
     // Corpo testo in relazione all'altezza della finestra
     textSize(height/20);
     textAlign(CENTER);
     textFont('Arvo');
-    text(hour(), width/2, height/3);
-    text(minute(), width/2, height/3*1.5);
-    text(second(), width/2, height/3*2);
+    text(hour(), 0, -90);
+    text(minute(), 0, 10);
+    text(second(), 0, 110);
     
-    pop();
     
     
     
