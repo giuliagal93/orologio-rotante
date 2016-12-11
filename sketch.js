13a433a3a00f4f322b12f95a76c8aab7db1cb8fc
giuliagal93 <peke@MacBook-Air.local>
2016-12-11 15:32:41 +0100

    10

--- sketch.js
+++ sketch.js
@@ -5,7 +5,8 @@ function setup() {
     
 }
 
-value = 90;
+var value = 90;
+var state = true;
 
 
 function deviceTurned() {
@@ -16,43 +17,15 @@ function deviceTurned() {
         value = 90;
     }
     
-    rotate(PI);
-    translate(0,-1*height/100);
-    // Corpo testo in relazione all'altezza della finestra
-    textSize(height/20);
-    textAlign(CENTER);
-    textFont('Arvo');
-    text(hour(), width/2, height/3);
-    text(minute(), width/2, height/3*1.5);
-    text(second(), width/2, height/3*2);
-    
-    /*
-    
-    background(hour()*21, minute()*4, second()*4);
-    push();
+    if (state == true) {
+        state = false;
+    } else if (state == false) {
+        state = true;
+    }
     
     
-    noStroke();
-    fill(255);
-    rotate(HALF_PI);
-    rectMode(CENTER);
-    rect(width/2,height/2-20,height/8,height/2)
-
-    fill(90);
-
-    translate(0,-1*height/100);
-    rotate(90);
-    // Corpo testo in relazione all'altezza della finestra
-    textSize(height/20);
-    textAlign(CENTER);
-    textFont('Trirong');
-    text(hour(), width/2, height/3);
-    textFont('Arvo');
-    text(minute(), width/2, height/3*1.5);
-    textFont('Work Sans')
-    text(second(), width/2, height/3*2);
     
-    pop();*/
+   
 }
 
 function draw() {
@@ -67,7 +40,13 @@ function draw() {
     rect(width/2,height/2-20,height/8,height/2)
 */
     fill(value);
-
+    
+    if (state == true) {
+        rotate(PI);
+    } else if (state == false) {
+        rotate(-PI);
+    }
+    
     translate(0,-1*height/100);
     // Corpo testo in relazione all'altezza della finestra
     textSize(height/20);
