648f2a03eca0a46486a43eb54f5f11a76257407f
giuliagal93 <peke@MacBook-Air.local>
2016-12-11 15:24:00 +0100

    08

--- sketch.js
+++ sketch.js
@@ -16,6 +16,15 @@ function deviceTurned() {
         value = 90;
     }
     
+    rotate(PI);
+    translate(0,-1*height/100);
+    // Corpo testo in relazione all'altezza della finestra
+    textSize(height/20);
+    textAlign(CENTER);
+    textFont('Arvo');
+    text(hour(), width/2, height/3);
+    text(minute(), width/2, height/3*1.5);
+    text(second(), width/2, height/3*2);
     
     /*
     
